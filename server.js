89be76195f4acb95b071f0bfe56d409ce2adf4b3
Tony Brown <tony@tonybrown.me>
2017-04-04 15:27:02 -0400

    using static html with EJS tags

--- server.js
+++ server.js
@@ -1,42 +1,21 @@
-/* Create HTTP server */
-import http from 'http';
+import config from './config';
+import apiRouter from './api';
+import express from 'express';
 
-const server = http.createServer((req, res) => {
-  res.write('Hello HTTP\n');
-  setTimeout(() => {
-    res.write('I can stream!\n');
-    res.end()
-  }, 3000)
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.get('/', (req, res) => {
+  //res.send('Hello, Express!');
+  res.render('index', {
+    content: '<h1>Hello Express and EJS!</h1>'
+  });
 });
 
-server.listen(8080);
+server.use('/api', apiRouter);
+server.use(express.static('public'));
 
-//createServer gives us an event emitter object
-// that we can subscribe to.
-// The most important emitted event is the request event
-// which gets emitted every time the http server receives a request
-//server.on('request', (req, res) => {
-  //res.write('Hello HTTP\n');
-  //setTimeout(() => {
-    //res.write('I can stream!\n');
-    //res.end()
-  //}, 3000)
-//});
-
-/* using HTTP as a server */
-
-
-/* Using HTTP or HTTPS as a client */
-//import https from 'https';
-
-//https.get('https://www.lynda.com', res => {
-  //console.log('Response status code: ', res.statusCode);
-
-  //res.on('data', chunk => {
-    //console.log(chunk.toString());
-  //});
-//});
-
-//import config, { nodeEnv, logStars } from './config';
-//console.log(config, nodeEnv);
-//logStars('My msg is, Node rocks')
+server.listen(config.port, () => {
+  console.log('Express is listening on port', config.port)
+});
