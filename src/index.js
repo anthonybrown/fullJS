f9c4a10c8baa8799da612f7873240bb7b87d6d05
Tony Brown <tony@tonybrown.me>
2017-04-05 08:17:07 -0400

    use json data to populate page with ReactJS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import data from './testData';
 import App from './components/App';
 
+//console.log(data);
+
 ReactDOM.render(
-  <App />,
+  <App contests={data.contests} />,
   document.getElementById('root')
 );
+
+// to test the componentWillUnmount lifecycle method
+//setTimeout(() => {
+  //ReactDOM.render(
+    //<h2 className='text-center'>New Component!</h2>,
+    //document.getElementById('root')
+  //);
+//}, 3000);
 
